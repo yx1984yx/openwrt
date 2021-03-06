876e4ac550daa514477b97a07910f628e3fe26cd
yx1984yx <yingxin.rick@outlook.com>
2021-03-06 11:12:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,11 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+
+
 
 
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
@@ -23,6 +27,6 @@ git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
 
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+#git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
