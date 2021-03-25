8f00dbec18f2e66b9b5e67ac14156a41f4a09e2b
yx1984yx <yingxin.rick@outlook.com>
2021-03-25 10:48:42 +0800

    Update diy-part1.sh

--- ImmortalWrt/diy-part1.sh
+++ ImmortalWrt/diy-part1.sh
@@ -12,28 +12,16 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-
-
-
-
-
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-
-#git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
-
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
